e89ce577aa48a8f9d1b618b69b6ba1f296fce020
abrhamadddis <abrhamaddis32@gmail.com>
2022-11-25 18:29:26 +0300

    form page

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
-from flask import Flask, render_template
+import sqlite3
+from flask import Flask, render_template, request, url_for, flash, redirect
 
 app = Flask(__name__)
 
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    conn = get_db_connection()
+    posts = conn.execute ('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('test_post.html', posts=posts)
 
 @app.route('/login/')
 def login():
@@ -23,4 +33,23 @@ def posts():
 def register():
     return render_template('register.html')
 
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        elif not content:
+            flash('Content is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                         (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('test_post'))
+
+    return render_template('test_register.html')
 app.run(debug=True)
--- init_db.py
+++ init_db.py
@@ -10,11 +10,18 @@ with open('schema.sql') as f:
 cur = connection.cursor()
 
 cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
-            ('First Post', 'Content for the first post')
+            ('Mac tools for absolute beginners', 'Without further ado, here are 7mac tools I use on all my macs. Whenever I get a new mac, I set up these seven tools so I can start working in a more comfortable atmosphere, and they help me to increase my productivity.')
             )
 
 cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
-            ('Second Post', 'Content for the second post')
+            ('Implementing A Second Brain in Emacs and Org-Mode', 'This post is a continuation of “Building A Second Brain with Emacs and Org-Mode.” If you haven’t read that yet, read that post first. It gave a high level overview of how BASB extends GTD, what Emacs and Org-Mode')
+
+            )
+cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
+            ('Mac tools for absolute beginners', 'Without further ado, here are 7mac tools I use on all my macs. Whenever I get a new mac, I set up these seven tools so I can start working in a more comfortable atmosphere, and they help me to increase my productivity.')
+            )
+cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
+            ('Mac tools for absolute beginners', 'Without further ado, here are 7mac tools I use on all my macs. Whenever I get a new mac, I set up these seven tools so I can start working in a more comfortable atmosphere, and they help me to increase my productivity.')
             )
 
 connection.commit()
