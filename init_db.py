f4c108eeac4018f19a27f36f0df85ffd179db8aa
melakualehegn <melakualehegn34@gmail.com>
2022-12-04 22:01:53 +0300

    database relationships

--- init_db.py
+++ init_db.py
@@ -9,17 +9,15 @@ with open('schema.sql') as f:
 
 cur = connection.cursor()
 
-cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
-            ('First Post', 'Content for the first post')
-            )
-
-cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
-            ('Second Post', 'Content for the second post')
-            )
-
 cur.execute("INSERT INTO user (name, email, password) VALUES (?, ?, ?)",
             ('alx', 'alxzone@gmail.com', 'alxzonego')
             )
+cur.execute("INSERT INTO posts (title, content, post_user_id) VALUES (?, ?, ?)",
+            ('First Post', 'Content for the first post', 1)
+            )
 
+cur.execute("INSERT INTO posts (title, content, post_user_id) VALUES (?, ?, ?)",
+            ('Second Post', 'Content for the second post', 1)
+            )
 connection.commit()
 connection.close()
